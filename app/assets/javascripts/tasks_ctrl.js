db7ae552e168dfab9590f6bf4d897a65cf60b1f8
Peter Jang <peter.x.jang@gmail.com>
2016-12-19 19:49:19 -0600

    Add example of input two-way binding

--- app/assets/javascripts/tasks_ctrl.js
+++ app/assets/javascripts/tasks_ctrl.js
@@ -3,5 +3,9 @@
 (function() {
   angular.module("app").controller("tasksCtrl", function($scope) {
     $scope.task1 = "Take out the garbage";
+    $scope.task2 = "Clean my boots";
+    $scope.task3 = "Shovel the snow";
+
+    // $scope.message = "Hello!";
   });
 })();
